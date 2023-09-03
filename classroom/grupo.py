0ca48c75cba2216b7e2343ea9f6d1d6e2b2c379e
Mnico-11 <141962291+Mnico-11@users.noreply.github.com>
2023-09-03 13:52:25 -0500

    mm

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,14 +3,12 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=[]):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
     def listadoAsignaturas(self, **kwargs):
-        if self._asignaturas is None:
-            self._asignaturas=[]
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
