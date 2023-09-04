c542817ff28e6c9d6912a53a640090df41abd58b
Mnico-11 <141962291+Mnico-11@users.noreply.github.com>
2023-09-03 19:57:09 -0500

    Update asignatura.py

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon="remoto"):
+    def __init__(self, nombre, salon=None):
         self._nombre = nombre
-        self._salon = salon
+        self._salon ="remoto" if not salon else salon
 
     def __str__(self):
         return self._nombre+ "," +self._salon    
